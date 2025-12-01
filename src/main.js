db4581c73fb2123f00f1734a5262a0f49fe51954
Mohamed El Ouadi <mohamed.elouadi@hbfs-cloud.com>
2025-12-01 14:08:29 +0100

    wip

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import vue3GoogleLogin from 'vue3-google-login'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import cors from "cors"
 
 const app = createApp(App)
-
 app.use(createPinia())
+app.use(cors({ origin: ["https://muqabala.online", "https://hbfs-cloud.github.io"] }))
+
 app.use(router)
 app.use(vue3GoogleLogin, {
     clientId: '222055895903-j0inv85m7qi7lt5l5nbvljoimr0tsb9k.apps.googleusercontent.com'
